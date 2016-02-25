aced076be6516c63165b685e3578a9c24ed284d0
fruitJazzy <li4drow@gmail.com>
2016-02-25 19:34:02 +0300

    init

---
+++ src/javase01/t01/main/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Hello World!");
+		//Calculator calc=new Calculator();
+		//System.out.println("2+2="+calc.sum(2,2));
+	}
+}
